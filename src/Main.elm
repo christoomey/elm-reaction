cc9a9f0b227c3889d7cdc3ba5f5fd74ddc4b94d0
Chris Toomey <chris@ctoomey.com>
2019-12-12 11:46:38 -0500

    Initial board model and render grid

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
 
 
 type Msg
@@ -12,19 +13,67 @@ type alias Flags =
     {}
 
 
+type Interactor
+    = One
+    | Two
+    | Three
+    | Four
+    | Arrow Direction
+
+
+type Direction
+    = Up
+    | Down
+    | Left
+    | Right
+
+
+type Projectile
+    = Projectile Direction
+
+
+type alias Board =
+    List (List (Maybe Interactor))
+
+
 type alias Model =
-    String
+    { board : Board
+    , projectiles : List Projectile
+    }
 
 
 view : Model -> Html Msg
-view model =
-    div []
-        [ text <| "Hello " ++ model ]
+view { board } =
+    div [ class "board" ] <| List.concatMap viewRow board
+
+
+viewRow : List (Maybe Interactor) -> List (Html Msg)
+viewRow row =
+    List.map viewCell row
+
+
+viewCell : Maybe Interactor -> Html Msg
+viewCell mInteractor =
+    case mInteractor of
+        Nothing ->
+            div [ class "cell" ] [ text "empty" ]
+
+        Just _ ->
+            div [ class "cell" ] [ text "interactor" ]
+
+
+initialBoard : Board
+initialBoard =
+    [ [ Nothing, Nothing, Just One, Nothing ]
+    , [ Nothing, Nothing, Nothing, Nothing ]
+    , [ Nothing, Nothing, Just One, Nothing ]
+    , [ Nothing, Nothing, Nothing, Nothing ]
+    ]
 
 
 init : Flags -> ( Model, Cmd Msg )
 init _ =
-    ( "Chris"
+    ( { board = initialBoard, projectiles = [] }
     , Cmd.none
     )
 
